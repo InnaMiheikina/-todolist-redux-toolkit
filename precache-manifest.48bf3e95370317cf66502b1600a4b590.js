28a5a7bdb485614b565c3f6d39a895676f615ba2
InnaMiheikina <InnaMiheikina93@gmail.com>
2023-04-01 21:49:08 +0300

    Updates

---
+++ precache-manifest.48bf3e95370317cf66502b1600a4b590.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8fcf2c0c9cce940db340b1314458624",
+    "url": "/-todolist-redux-toolkit/index.html"
+  },
+  {
+    "revision": "fefb57f38264e9aa4e83",
+    "url": "/-todolist-redux-toolkit/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "eedc512af3c91e6cc62b",
+    "url": "/-todolist-redux-toolkit/static/js/2.317bd65b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/-todolist-redux-toolkit/static/js/2.317bd65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fefb57f38264e9aa4e83",
+    "url": "/-todolist-redux-toolkit/static/js/main.4efc893c.chunk.js"
+  },
+  {
+    "revision": "043a8a2d60439bef9599",
+    "url": "/-todolist-redux-toolkit/static/js/runtime-main.e8baa5ba.js"
+  }
+]);
